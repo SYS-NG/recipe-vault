bee52eabc24134875624a311de0bcda3a40f9bf5
Steven Ng <szeyungng@gmail.com>
2021-11-13 16:07:06 -0700

    basic vision setup

--- camera.py
+++ camera.py
@@ -1,6 +1,9 @@
+import io
+import os
 import RPi.GPIO as GPIO
 from picamera import PiCamera
 from time import sleep
+from google.cloud import vision
 
 class cameraDriver():
     def __init__(self):
@@ -8,7 +11,7 @@ class cameraDriver():
     
     @staticmethod
     def _getGPIOConfig():
-        """Raspberry Pi GPIO"""
+        """Raspberry Pi GPIO Config"""
         GPIOMode = GPIO.BCM
         CaptureButtonPin = 17
         return {
@@ -16,14 +19,24 @@ class cameraDriver():
             'capture' : CaptureButtonPin
         }
 
+    @staticmethod
+    def _getVisionConfig():
+        """Google Vision Cred Location"""
+        return 'rv-cred/json'
+
     def _captureImage(self):
         self.camera.capture('./cameraTest.jpg')
-        
     
     def drive(self):
+
+        #Set up RPi GPIO
         RPiConfig = self._getGPIOConfig()
         GPIO.setmode(RPiConfig['mode'])
         GPIO.setup(RPiConfig['capture'], GPIO.IN)
+
+        #Set up google client
+        credFile = self._getVisionConfig()
+        vision_client = vision.ImageAnnotatorClient(keyFilename=credFile)
 
         self.camera.start_preview(alpha = 180)
         sleep(2)
